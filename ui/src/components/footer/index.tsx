10f1f38f2494804f0bba148a8551a9e1a30f1127
Abdallah Hodieb <hodieba@amazon.com>
2021-05-09 00:21:18 +0100

    🎨 update icons and remove font-awesome

--- ui/src/components/footer/index.tsx
+++ ui/src/components/footer/index.tsx
@@ -9,38 +9,36 @@ const Footer: React.FunctionComponent = () => {
                 <p className="is-size-6">
                     <span className="icon-text">
                         <span className="icon">
-                            <i className="fas fa-laptop-code"></i>
+                            <svg className="svg-icon">
+                                <use xlinkHref="#icon-code "></use>
+                            </svg>
                         </span>
                         <span>A <a href="https://www.oreilly.com/library/view/apprenticeship-patterns/9780596806842/ch05s03.html" target="_blank" rel="noopener noreferrer">toy project</a> by: Abdallah Hodieb</span>
                     </span>
                 </p>
                 <p className="is-size-3">
                     <span className="px-3">
-                        <a href="http://github.com/ahodieb" target="_blank" rel="noopener noreferrer">
-                            <span className="icon">
-                                <i className="fab fa-github"></i>
-                            </span>
+                        <a href="https://twitter.com/abdallahhodieb" target="_blank" rel="noopener noreferrer">
+                            <svg className="svg-icon is-2">
+                                <use xlinkHref="#icon-twitter"></use>
+                            </svg>
                         </a>
                     </span>
-
                     <span className="px-3">
-                        <a href="http://github.com/ahodieb" target="_blank" rel="noopener noreferrer">
-                            <span className="icon">
-
-                                <i className="fab fa-twitter"></i>
-                            </span>
+                        <a href="https://github.com/ahodieb/lttl.xyz" target="_blank" rel="noopener noreferrer">
+                            <svg className="svg-icon is-">
+                                <use xlinkHref="#icon-github"></use>
+                            </svg>
                         </a>
                     </span>
                 </p>
-
                 <p className="is-size-7">
-                    <span className="icon is-size-6">
-                        <i className="fas fa-cat"></i>
-                    </span>
-                    <a href='https://www.getillustrations.com/single-illustration/Explore-Travel-Web-Online-Space-Cat'>Abstrike illustrations</a>
+                    <a href='https://www.freepik.com/vectors/water'>"Whale" by catalyststuff - www.freepik.com</a>
+                </p>
+                <p className="is-size-7">
+                    <a href='https://fontawesome.com/license'>Icons from fontawesome</a>
                 </p>
             </div>
-
         </footer>);
 }
 
